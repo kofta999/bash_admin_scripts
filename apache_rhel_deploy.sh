4f6b5b0deac6bd38fc33d6453e75d8d62f2c9c2e
kofta999 <mostafaxxx555@gmail.com>
2023-03-11 10:50:39 +0200

    initial commit

---
+++ apache_rhel_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO
+# 1-Get the commands used to run the server the organise it there
+# 2-Use heredoc or so to write out to the config files
+# 3-Add option to pick between Debian-based or RHEL-based configs
+# 4-Testing and debugging
